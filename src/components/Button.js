74994bd352a81a58e4a79e4909b3cef4b7d1705d
Emmanuel <enobakhare.emmanuel@gmail.com>
2022-10-28 21:26:44 +0100

    initial link tree development

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Button = ({ title }) => {
+  return (
+    <button className="h-16 bg-stale border w-full rounded-lg">{title}</button>
+  );
+};
+
+export default Button;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#101828",
+        stale: "#EAECF0",
+      },
+    },
   },
   plugins: [],
 };
